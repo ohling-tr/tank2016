00b70b595b32dc9dd6610a3f0d3ddad0487773e1
Tim Ohling <trohling@peak.org>
2016-02-03 19:45:43 -0800

    setup to call method for tank drive

--- Tank2016/src/org/usfirst/frc4662/Tank2016/commands/TankDrive.java
+++ Tank2016/src/org/usfirst/frc4662/Tank2016/commands/TankDrive.java
@@ -42,6 +42,7 @@ public class TankDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	//drive method goes here with joysticks from default
     }
 
     // Make this return true when this Command no longer needs to run execute()
